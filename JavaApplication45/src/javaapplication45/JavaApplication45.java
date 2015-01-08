c0f7e6f3de48f6b78a1fcc0fe5a1a95dca2a0043
ShadowXuelo <danielxuelo@gmail.com>
2015-01-08 21:22:04 +0100

    Update JavaApplication45.java

--- JavaApplication45/src/javaapplication45/JavaApplication45.java
+++ JavaApplication45/src/javaapplication45/JavaApplication45.java
@@ -17,7 +17,7 @@ public class JavaApplication45 {
      */
     public static void main(String[] args) {
         Scanner introducir =new Scanner(System.in);
-        int num, num2, opt;
+        int num, num2, opt, igualador, contador, igualador2;
         double raiz;
         opt=introducir.nextInt();
         switch (opt){
@@ -28,17 +28,41 @@ public class JavaApplication45 {
                 System.out.println(num);
                 break;
             case 2:
+                num=introducir.nextInt();
+                num2=introducir.nextInt();
+                num=num-num2;
+                System.out.println(num);
+                break;
+                
             case 3:
                 num=introducir.nextInt();
                 num2=introducir.nextInt();
                 num=num*num2;
                 System.out.println(num);
                 break;
-            case 4: 
+            case 4:
+                num=introducir.nextInt();
+                num2=introducir.nextInt();
+                num=num/num2;
+                System.out.println(num);
+                break;
+            case 5: 
                 raiz=introducir.nextInt();
                 raiz=Math.sqrt(raiz);
                 System.out.println(raiz);
                 break;
+            case 6:
+                num=introducir.nextInt();
+                num2=introducir.nextInt();
+                igualador=num;
+                igualador2=num2;
+                do {
+                num=num*igualador;
+                contador++;
+                } while (igualador2==contador);
+                System.out.println(num);
+                break;
+             
         }
         
     }
